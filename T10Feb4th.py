4347a78de917e05954dc304749fa619b80897f49
SkyBoundRonak <64765886+SkyBoundRonak@users.noreply.github.com>
2025-02-05 11:44:24 +0530

    T10Feb4th.py

--- T10Feb4th.py
+++ T10Feb4th.py
@@ -3,7 +3,7 @@ def generate_squares():
     return [i**2 for i in range(1, 11)]  # List comprehension
 
 def main():
-    # Generate the list of squares
+    # Generating the list of squares
     squares = generate_squares()
     # Print the result
     print(squares)
