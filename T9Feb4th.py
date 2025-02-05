817e2bcf53b17ba76223555b4c34fa233125d1a4
SkyBoundRonak <64765886+SkyBoundRonak@users.noreply.github.com>
2025-02-05 11:59:16 +0530

    T9Feb4th.py

--- T9Feb4th.py
+++ T9Feb4th.py
@@ -1,13 +1,10 @@
 #Task 9: Calculate the Average of a List of Numbers
 def calculate_average(numbers):
-    return sum(numbers) / len(numbers)  # Sum divided by count
+    return sum(numbers) / len(numbers)  
 
-def main():
-    # Input list
-    numbers = [2, 4, 6, 8, 10]
-    # Calculate the average
+def main():   
+   numbers = [2, 4, 6, 8, 10]
     average = calculate_average(numbers)
-    # Print the result
     print(f"Average of the list: {average}")
 
 if __name__ == "__main__":
