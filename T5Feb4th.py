a05371741cf60861bc9513a9347eb67c98ce9ba2
SkyBoundRonak <64765886+SkyBoundRonak@users.noreply.github.com>
2025-02-05 11:55:51 +0530

    T5Feb4th.py

--- T5Feb4th.py
+++ T5Feb4th.py
@@ -1,8 +1,7 @@
 #Task 5: Count the Frequency of Words in a Sentence
 def count_word_frequency(sentence):
-    # Split the sentence into words
     words = sentence.split()
-    # Create a dictionary to store word frequencies
+
     frequency = {}
     for word in words:
         if word in frequency:
@@ -12,11 +11,11 @@ def count_word_frequency(sentence):
     return frequency
 
 def main():
-    # Input sentence
+    
     sentence = "apple banana apple grapes apple"
-    # Count word frequencies
+  
     word_frequency = count_word_frequency(sentence)
-    # Print the result
+
     print(word_frequency)
 
 if __name__ == "__main__":
