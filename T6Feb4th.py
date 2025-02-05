d5bd010b244226a9f20a968c358639f1fd98e55a
SkyBoundRonak <64765886+SkyBoundRonak@users.noreply.github.com>
2025-02-05 11:56:34 +0530

    T6Feb4th.py

--- T6Feb4th.py
+++ T6Feb4th.py
@@ -1,15 +1,15 @@
 #Task 6: Find the Intersection of Two Sets
 def find_intersection(set1, set2):
-    # Use the intersection() method or the & operator
+
     return set1.intersection(set2)
 
 def main():
     # Input sets
     set1 = {1, 2, 3}
     set2 = {2, 3, 4}
-    # Find intersection
+
     intersection = find_intersection(set1, set2)
-    # Print the result
+ 
     print(intersection)
 
 if __name__ == "__main__":
